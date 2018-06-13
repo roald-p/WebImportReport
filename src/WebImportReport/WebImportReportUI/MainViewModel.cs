f1a864d3f18cb1ed55ad1a7f4845303ab6f760f1
Roald Parelius <rpa@dips.no>
2018-06-13 14:04:40 +0200

    open folder command

--- src/WebImportReport/WebImportReportUI/MainViewModel.cs
+++ src/WebImportReport/WebImportReportUI/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
@@ -20,11 +21,14 @@ namespace WebImportReportUI
 
         private DelegateCommand m_selectPathCommand;
         private DelegateCommand m_generateCSVCommand;
+        private DelegateCommand m_openFolderCommand;
 
         private string m_inputDir;
         private string m_filePattern= "paamelding*.xml";
         private string m_matchingFiles;
         private string m_convertResult;
+        private bool m_directoryExists;
+
         public string InputDir
         {
             get => m_inputDir;
@@ -32,6 +36,7 @@ namespace WebImportReportUI
             {
                 m_inputDir = value;
                 OnPropertyChanged(nameof(InputDir));
+                OnPropertyChanged(nameof(InputDirectoryExists));
                 MatchingFiles = FoundFiles();
             }
         }
@@ -93,6 +98,19 @@ namespace WebImportReportUI
             }
         }
 
+        public ICommand OpenFolderCommand
+        {
+            get
+            {
+                if (m_openFolderCommand == null)
+                {
+                    m_openFolderCommand = new DelegateCommand(this.OpenFolderCommandExecute);
+                }
+
+                return m_openFolderCommand;
+            }
+        }
+        
         public void SelectPathCommandExecute()
         {
             using (var dlg = new FolderBrowserDialog())
@@ -106,6 +124,19 @@ namespace WebImportReportUI
                     Properties.Settings.Default.Folder_Path = dlg.SelectedPath;
                     Properties.Settings.Default.Save();
                 }
+            }
+        }
+
+        public void OpenFolderCommandExecute()
+        {
+            Process.Start(m_inputDir);
+        }
+
+        public bool InputDirectoryExists
+        {
+            get
+            {
+                return Directory.Exists(m_inputDir);
             }
         }
 
